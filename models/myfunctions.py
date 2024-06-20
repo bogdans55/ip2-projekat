c4ea02caec5f57703fe5e19abc13891ba8490244
kerkoc01 <stefan.kerkoc@gmail.com>
2024-06-20 18:50:18 +0200

    More refactoring

--- models/myfunctions.py
+++ models/myfunctions.py
@@ -18,6 +18,9 @@ from sklearn.linear_model import LogisticRegression
 def replace_values(y):
     return y.replace({'B': 0, 'D': 4, 'M': 1, 'NK': 2, 'T': 3}, regex=True)
 
+def return_values(y):
+    return y.replace({0: 'B', 4: 'D', 1: 'M', 2: 'NK', 3: 'T'}, regex=True)
+
 def model_info(y_real, y_pred, title, labels=['B', 'D', 'M', 'NK', 'T']):
     cm = confusion_matrix(y_real, y_pred)
     class_sums = cm.sum(axis=1)
@@ -59,16 +62,15 @@ def test(data, algorithm, model, params, title, classes=['B', 'D', 'M', 'NK', 'T
     results = pd.DataFrame()
     results[title + '_self'] = test_on_self(data, params, algorithm, title + '_self', classes)
     results[title] = load_test(data, model, title)
+    if(algorithm == XGBClassifier):
+        results[title + '_self'] = return_values(results[title + '_self'])
+        results[title] = return_values(results[title])
     return results
 
 def write_results(filename, classes):
     datapath = '../data/preprocessed_data_' + filename + '.csv'
-    resultspath = '../data/results_' + filename + '.csv'
     data = pd.read_csv(datapath)
-    if os.path.exists(resultspath) and os.path.getsize(resultspath) > 0:
-        results = pd.read_csv(resultspath)
-    else:
-        results = pd.DataFrame()
+    results = pd.DataFrame()
 
     rf = load('rf.joblib')
     rf_over = load('rf_over.joblib')
@@ -78,8 +80,8 @@ def write_results(filename, classes):
     'min_samples_split': 5,
     'n_estimators': 300}
 
-    results = pd.concat([results ,test(data, RandomForestClassifier, rf, rf_params, 'rf', classes)], ignore_index=True)
-    results = pd.concat([results ,test(data, RandomForestClassifier, rf_over, rf_params, 'rf_over', classes)], ignore_index=True)
+    results = pd.concat([results ,test(data, RandomForestClassifier, rf, rf_params, 'rf', classes)], axis=1)
+    results = pd.concat([results ,test(data, RandomForestClassifier, rf_over, rf_params, 'rf_over', classes)], axis=1)
 
     log_reg = load('log_reg.joblib')
     log_reg_cw = load('log_reg_cw.joblib')
@@ -88,9 +90,9 @@ def write_results(filename, classes):
     log_reg_params = {'C': 0.001, 'penalty': 'l2', 'solver': 'sag'}
     log_reg_cw_params = {'C': 0.001, 'penalty': 'l2', 'solver': 'sag', 'class_weight' : 'balanced'}
 
-    results = pd.concat([results ,test(data, LogisticRegression, log_reg, log_reg_params, 'log_reg', classes)], ignore_index=True)
-    results = pd.concat([results ,test(data, LogisticRegression, log_reg_cw, log_reg_cw_params, 'log_reg_cw', classes)], ignore_index=True)
-    results = pd.concat([results ,test(data, LogisticRegression, log_reg_over, log_reg_params, 'log_reg_over', classes)], ignore_index=True)
+    results = pd.concat([results ,test(data, LogisticRegression, log_reg, log_reg_params,'log_reg', classes)], axis=1)
+    results = pd.concat([results ,test(data, LogisticRegression, log_reg_cw, log_reg_cw_params,'log_reg_cw', classes)], axis=1)
+    results = pd.concat([results ,test(data, LogisticRegression, log_reg_over, log_reg_params,'log_reg_over', classes)], axis=1)
 
     nn = load('nn.joblib')
     nn_over = load('nn_over.joblib')
@@ -101,15 +103,15 @@ def write_results(filename, classes):
     'learning_rate_init': 0.001,
     'max_iter': 1000}
 
-    results = pd.concat([results ,test(data, MLPClassifier, nn, nn_params, 'nn', classes)], ignore_index=True)
-    results = pd.concat([results ,test(data, MLPClassifier, nn_over, nn_params, 'nn_over', classes)], ignore_index=True)
+    results = pd.concat([results ,test(data, MLPClassifier, nn, nn_params,'nn', classes)], axis=1)
+    results = pd.concat([results ,test(data, MLPClassifier, nn_over, nn_params,'nn_over', classes)], axis=1)
 
     xg = load('xg.joblib')
     xg_over = load('xg_over.joblib')
 
     xg_params = {'learning_rate': 0.2, 'max_depth': 4, 'n_estimators': 300}
 
-    results = pd.concat([results ,test(data, XGBClassifier, xg, xg_params, 'xg')], ignore_index=True)
-    results = pd.concat([results ,test(data, XGBClassifier, xg_over, xg_params, 'xg_over')], ignore_index=True)
+    results = pd.concat([results ,test(data, XGBClassifier, xg, xg_params,'xg')], axis=1)
+    results = pd.concat([results ,test(data, XGBClassifier, xg_over, xg_params,'xg_over')], axis=1)
 
     return results
